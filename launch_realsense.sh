9dfb5401c68c32b497890eabd85a13e20c6dc028
arima <arijun0307@gmail.com>
2020-11-12 16:14:41 +0900

    enable to set ROS_MASTER_URI

--- launch_realsense.sh
+++ launch_realsense.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ROS_DISTRO=melodic
 IMAGE_NAME=ghcr.io/amslabtech/realsense_ros:${ROS_DISTRO}
@@ -8,14 +8,23 @@ echo "CONTAINER_NAME=${CONTAINER_NAME}"
 
 ROS_MASTER_URI="http://`hostname -I | cut -d' ' -f1`:11311"
 ROS_IP=`hostname -I | cut -d' ' -f1`
+LAUNCH=rs_camera.launch
+
+if [ ! $# -eq 0 ]; then
+    IP_CHECK=$(echo $1 | egrep "^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+    if [ "${IP_CHECK}" ]; then
+        ROS_MASTER_URI="http://$1:11311"
+        if [ $# -ge 2 ]; then
+            LAUNCH=${@:2}
+        fi
+    else
+        LAUNCH=$@
+    fi
+fi
+
 echo "ROS_MASTER_URI=${ROS_MASTER_URI}"
 echo "ROS_IP=${ROS_IP}"
-
-if [ $# -eq 0 ]; then
-    LAUNCH=rs_camera.launch
-else
-    LAUNCH=$1
-fi
+echo "LAUNCH=${LAUNCH}"
 
 docker run -it --rm \
     --privileged \
